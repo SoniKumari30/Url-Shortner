918cae350b6956164b4de2ce81e3f37009254e04
SoniKumari <sonikumarisharma30@gmail.com>
2020-09-18 02:24:50 +0530

    update

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express= require('express')
-const app=express()
-const router=require('./config/routes')
-const configureDB=require('./config/database')
+const express = require('express')
+const app = express()
+const configureDB = require('./config/database')
+const router = require('./config/routes')
+const port = 3099
 
 
 configureDB()
 app.use(express.json())
 app.use(router)
-const port=3033
+ 
 
-
-
-app.listen(port,()=>{
-    console.log('server running on port',port)
+app.listen(port, () => {
+    console.log('server is running on port', port)
 })
